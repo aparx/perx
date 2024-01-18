56ee73c54944c2ee0e808fba833824bd34741a93
aparx <undestroy1310@gmail.com>
2024-01-18 01:55:23 +0100

    Fixed hashcode problem in command args

--- src/main/java/io/github/aparx/perx/command/args/CommandArgumentList.java
+++ src/main/java/io/github/aparx/perx/command/args/CommandArgumentList.java
@@ -61,7 +61,7 @@ public final class CommandArgumentList implements Iterable<CommandArgument> {
 
   private static int hashCode(String[] args, int offset, int length) {
     int hashCode = 0;
-    for (int i = offset; i < length && i < args.length; ++i)
+    for (int i = offset; i < offset + length && i < args.length; ++i)
       hashCode = 31 * hashCode + args[i].hashCode();
     return hashCode;
   }
